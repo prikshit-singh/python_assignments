aa595c7ae9c5a910381fd5a01df4d42733349ebf
prikshit-singh <prikshitlatherlather@gmail.com>
2022-06-15 11:54:05 +0530

    first commit

---
+++ customizingboxplot.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+np.random.seed(10)
+data1 = np.random.normal(100,20,200)
+data2 = np.random.normal(100,20,200)
+data3 = np.random.normal(100,20,200)
+data4 = np.random.normal(100,20,200)
+
+data = [data1,data2,data3,data4]
+
+fig = plt.figure(figsize = (10,7))
+ax = fig.add_axes([0,0,1,1])
+bp = ax.boxplot(data)
+plt.show()
